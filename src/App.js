014dce72747b0e8573462754667add3ec9d3a54e
dingruos <Drose@2015>
2020-03-02 16:17:27 -0500

    modifidied

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
+
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer';
 
